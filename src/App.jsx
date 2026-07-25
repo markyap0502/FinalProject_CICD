5969ec56cc0bdec0040924b724110b9aefe0d6d9
Mark Yap <mark.yap@ciit.edu.ph>
2026-07-25 08:24:55 +0800

    Jenkins Lint Test success

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ const LoginPage = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const uselessValue = "42";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
